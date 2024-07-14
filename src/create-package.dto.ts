c828848ec35a6db6ffbd3e5516d4fa9d065eb042
rodrigo serrano <kiradex21@gmail.com>
2024-07-13 19:31:43 -0600

    WIP BE

--- src/create-package.dto.ts
+++ src/create-package.dto.ts
@@ -1,7 +1,23 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreatePackageDto {
+  @IsString()
+  @IsNotEmpty()
   content: string;
+
+  @IsString()
+  @IsNotEmpty()
   height: string;
+
+  @IsString()
+  @IsNotEmpty()
   length: string;
+
+  @IsString()
+  @IsNotEmpty()
   weight: string;
+
+  @IsString()
+  @IsNotEmpty()
   width: string;
 }
